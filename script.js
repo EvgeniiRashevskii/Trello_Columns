6c1c0d7b140983d1fffa59971b70de943e70df97
eugenyrashevsky <rashevskii221@gmail.com>
2024-11-19 18:25:33 +0200

    Table Drag and Drop

---
+++ script.js
@@ -0,0 +1,21 @@
+
+document.querySelectorAll('.column').forEach(Column.process)
+
+document.querySelector('[data-action-addColumn]').addEventListener('click', function (event) {
+    const columnElement = document.createElement('div');
+    columnElement.classList.add('column');
+    columnElement.setAttribute('draggable', 'true');
+    columnElement.setAttribute('data-column-id', Column.idCounter);
+    Column.idCounter++;
+    columnElement.innerHTML = `<p class="column-header" contenteditable="true">В плане</p>
+					<div data-notes>
+					</div>
+					<p class="column-footer">
+						<span data-action-addNote class="action">+ Добавить карточку</span>
+					</p>`
+    document.querySelector('.columns').append(columnElement);
+    Column.process(columnElement);
+})
+
+document.querySelectorAll('.note').forEach(Note.process);
+
