70c52b2db2b2fcd5423aa863a767e99388692eef
eugenyrashevsky <rashevskii221@gmail.com>
2024-11-25 22:31:52 +0200

    Added code objects in the column

--- application.js
+++ application.js
@@ -1,16 +1,42 @@
 const Application = {
     save() {
-        const oject = {
-            columns:{
-                idCounter:1,
-                items:[]
+        const object = {
+            columns: {
+                idCounter: Column.idCounter,
+                items: []
             },
-            notes:{
-                idCounter:1,
-                items:[]
+            notes: {
+                idCounter: Note.idCounter,
+                items: []
             }
         }
-        document.
+        document
+            .querySelectorAll('.column')
+            .forEach(columnElement => {
+                const column = {
+                    //parseInt оборачиваем в число
+                    id: parseInt(columnElement.getAttribute('data-column-id')),
+                    noteIds: []
+                }
+                columnElement
+                    .querySelectorAll('note')
+                    .forEach(noteElement => {
+                        column.noteIds.push(parseInt(noteElement.getAttribute('data-note-id')))
+                    })
+                object.columns.items.push(column)
+            })
+
+        document
+            .querySelectorAll('.note')
+            .forEach(noteElement => {
+                const note = {
+                    id: parseInt(noteElement.getAttribute('data-note-id')),
+                    content: noteElement.textContent
+                }
+                object.notes.items.push(note)
+            })
+
+        return object;
     },
 
     load() {
